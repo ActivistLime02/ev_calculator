9bebda27cd35eb30b89128ba482f21bfea4aa7e2
ActivistLime02 <27824978+ActivistLime02@users.noreply.github.com>
2026-04-22 19:35:10 +0200

    This is the proper way to hoist link to head

--- src/components/default_layout.rs
+++ src/components/default_layout.rs
@@ -15,7 +15,7 @@ pub fn DefaultLayout() -> Element {
 
         // Acording to a github issue, this should behave like react 19.
         // Hoist to head
-        link {
+        document::Link {
             rel: "stylesheet",
             href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css",
             integrity: "sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==",
