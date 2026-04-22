393991d40af7b2b92a2a67a9cc246a4534ec2c62
ActivistLime02 <27824978+ActivistLime02@users.noreply.github.com>
2026-04-22 18:26:30 +0200

    Font Awesome in default layout

--- src/components/default_layout.rs
+++ src/components/default_layout.rs
@@ -13,6 +13,16 @@ pub fn DefaultLayout() -> Element {
             href: TAILWIND_CSS
         }
 
+        // Acording to a github issue, this should behave like react 19.
+        // Hoist to head
+        link {
+            rel: "stylesheet",
+            href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css",
+            integrity: "sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==",
+            crossorigin: "anonymous",
+            referrerpolicy: "no-referrer"
+         }
+
         div {
             class: "bg-ctp-base text-ctp-text latte",
             Header {}
