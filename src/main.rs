ef370c712e26091ef6642d3169762f758d559673
ActivistLime02 <27824978+ActivistLime02@users.noreply.github.com>
2026-04-25 12:28:46 +0200

    Applied not found page to routes

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,13 @@ fn App() -> Element {
 
 #[derive(Routable, Clone, PartialEq)]
 enum Route {
+    // Layout applies to all the routes below, apparently.
     #[layout(DefaultLayout)]
     #[route("/")]
     Calculator,
 
     // Catch-all route. Returns 404 when the user accesses a page that does not exist.
-    // #[route("/:..segments")]
-    // PageNotFound { segments: Vec<String> },
+    // Must be at the end, this enum works like a firewall rule.
+    #[route("/:..segments")]
+    NotFound { segments: Vec<String> },
 }
