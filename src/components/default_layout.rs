666a0bc3db0d6122d9396a833d2a2268263448e0
ActivistLime02 <27824978+ActivistLime02@users.noreply.github.com>
2026-04-26 08:45:10 +0200

    Remove cdn for font awesome

--- src/components/default_layout.rs
+++ src/components/default_layout.rs
@@ -17,14 +17,6 @@ pub fn DefaultLayout() -> Element {
 
         // Acording to a github issue, this should behave like react 19.
         // Hoist to head
-        document::Link {
-            rel: "stylesheet",
-            href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css",
-            integrity: "sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==",
-            crossorigin: "anonymous",
-            referrerpolicy: "no-referrer"
-        }
-
         document::Title {
             "EV Charge Time Calculator"
         }
